442abed4a53ef98df26dd8a39eff362688cd6efd
Austin <smith.austin813@gmail.com>
2023-03-14 12:53:49 -0700

    Refactored for easier further development.

--- hello_app/views.py
+++ hello_app/views.py
@@ -1,26 +1,19 @@
-import re
-from datetime import datetime
-
 from flask import Flask
 from flask import render_template
-
-app = Flask(__name__)
-
+from datetime import datetime
+from . import app
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
-
 @app.route("/about/")
 def about():
     return render_template("about.html")
 
-
-@app.route("/contact")
+@app.route("/contact/")
 def contact():
     return render_template("contact.html")
-
 
 @app.route("/hello/")
 @app.route("/hello/<name>")
@@ -28,9 +21,8 @@ def hello_there(name = None):
     return render_template(
         "hello_there.html",
         name=name,
-        date=datetime.now(),
+        date=datetime.now()
     )
-
 
 @app.route("/api/data")
 def get_data():
